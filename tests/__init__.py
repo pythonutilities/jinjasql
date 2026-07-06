b93e54acbe3074aeae9e8a7d7ba8c668b7a158f8
PandaWhoCodes <ufoundashish@gmail.com>
2026-07-06 13:47:42 +0530

    test: fix test suite execution, docker constraints, and add regression tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,16 +1,12 @@
-import sys
 import unittest
+
 from tests.test_jinjasql import JinjaSqlTest
 from tests.test_real_database import PostgresTest, MySqlTest
 
+
 def all_tests():
+    loader = unittest.TestLoader()
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(JinjaSqlTest))
-    suite.addTest(unittest.makeSuite(PostgresTest))
-    suite.addTest(unittest.makeSuite(MySqlTest))
-
-    if sys.version_info <= (3, 4):
-        from tests.test_django import DjangoTest
-        suite.addTest(unittest.makeSuite(DjangoTest))
-
+    for test_case in (JinjaSqlTest, PostgresTest, MySqlTest):
+        suite.addTests(loader.loadTestsFromTestCase(test_case))
     return suite
