efdb0db85368e514e4a3a303d37d15861edd73a1
PandaWhoCodes <ufoundashish@gmail.com>
2026-07-06 13:47:30 +0530

    build(deps): bump Jinja2 floor to >=3.1.6 to mitigate CVE-2024-56326 and others

--- setup.py
+++ setup.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python
 import os
+import re
 
-# Duplicating version from jinjasql.__init__.py
-# We can't directly import it from jinjasql,
-# because during installation Jinja2 isn't installed as yet
-# 
-# There are several approaches to eliminate this redundancy,
-# see https://packaging.python.org/single_source_version/
-# but for now, we will simply maintain it in two places
-__version__ = '0.1.12'
+from setuptools import setup
 
-long_description = '''
-Generate SQL Queries using a Jinja Template, without worrying about SQL Injection
+# Read the version from jinjasql/__init__.py, so that it is
+# maintained in exactly one place. We can't import jinjasql here,
+# because during installation Jinja2 isn't installed as yet.
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, 'jinjasql', '__init__.py')) as f:
+    __version__ = re.search(r"__version__\s*=\s*'([^']+)'", f.read()).group(1)
 
-JinjaSQL automatically binds parameters that are inserted into the template.
-After JinjaSQL evaluates the template, you get 1) Query with placeholders
-for parameters, and 2) List of values that need to be bound to the query. 
-
-JinjaSQL doesn't actually execute the query - it only prepares the 
-query and the bind parameters. You can execute the query using any 
-database engine / driver you are working with.
-
-'''
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
 
 sdict = {
     'name': 'jinjasql2',
     'version': __version__,
     'description': 'Generate SQL Queries and Corresponding Bind Parameters using a Jinja2 Template',
     'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
     'url': 'https://github.com/pythonutilities/jinjasql',
-    'download_url': f'http://cloud.github.com/downloads/pythonutilities/jinjasql/jinjasql-{__version__}.tar.gz',
     'author': 'Sridhar, Thomas Ashish Cherian, Sripathi Krishnan',
     'author_email': 'crsridhar23@gmail.com',
     'maintainer': 'Sridhar, Thomas Ashish Cherian',
@@ -37,24 +28,22 @@ sdict = {
     'keywords': ['Jinja2', 'SQL', 'Python', 'Template'],
     'license': 'MIT',
     'packages': ['jinjasql'],
-    'test_suite': 'tests.all_tests',
-    'install_requires': ['Jinja2>=2.11.3'],
+    'python_requires': '>=3.8',
+    'install_requires': ['Jinja2>=3.1.6'],
     'classifiers': [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 }
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
 setup(**sdict)
-
